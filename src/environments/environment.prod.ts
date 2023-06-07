7f2ba3c8242375e1e928efb4be43953f1d19d5a4
uk0724 <udaykumar.ab2017@gmail.com>
2023-06-07 14:36:53 +0530

    Add firebase env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAufrFfxV2yq8ZY2UJm0lKVZyC8aTPfWAI",
+    authDomain: "new23-768df.firebaseapp.com",
+    databaseURL: "https://new23-768df-default-rtdb.firebaseio.com",
+    projectId: "new23-768df",
+    storageBucket: "new23-768df.appspot.com",
+    messagingSenderId: "477434350700",
+    appId: "1:477434350700:web:43f449516c033decc91c49"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,13 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyCJgkioYRB68fopHSQKAvdGLn381VSLHYM",
-    authDomain: "angularcrud-6be69.firebaseapp.com",
-    databaseURL: "https://angularcrud-6be69-default-rtdb.firebaseio.com",
-    projectId: "angularcrud-6be69",
-    storageBucket: "angularcrud-6be69.appspot.com",
-    messagingSenderId: "535243672600",
-    appId: "1:535243672600:web:c40188e462143f6225076d"
-  }
+  apiKey: "AIzaSyAufrFfxV2yq8ZY2UJm0lKVZyC8aTPfWAI",
+  authDomain: "new23-768df.firebaseapp.com",
+  databaseURL: "https://new23-768df-default-rtdb.firebaseio.com",
+  projectId: "new23-768df",
+  storageBucket: "new23-768df.appspot.com",
+  messagingSenderId: "477434350700",
+  appId: "1:477434350700:web:43f449516c033decc91c49"
 };
 
 /*
