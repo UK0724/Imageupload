7ca548eed9045f30e45b50b99ec74da689b30c63
uk0724 <udaykumar.ab2017@gmail.com>
2023-06-07 14:38:43 +0530

    add intial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,10 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyAufrFfxV2yq8ZY2UJm0lKVZyC8aTPfWAI",
-    authDomain: "new23-768df.firebaseapp.com",
-    databaseURL: "https://new23-768df-default-rtdb.firebaseio.com",
-    projectId: "new23-768df",
-    storageBucket: "new23-768df.appspot.com",
-    messagingSenderId: "477434350700",
-    appId: "1:477434350700:web:43f449516c033decc91c49"
-  }
+  firebaseConfig : { apiKey: "AIzaSyAufrFfxV2yq8ZY2UJm0lKVZyC8aTPfWAI",
+  authDomain: "new23-768df.firebaseapp.com",
+  databaseURL: "https://new23-768df-default-rtdb.firebaseio.com",
+  projectId: "new23-768df",
+  storageBucket: "new23-768df.appspot.com",
+  messagingSenderId: "477434350700",
+  appId: "1:477434350700:web:3c31b64ee25f8a0dc91c49" }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  apiKey: "AIzaSyAufrFfxV2yq8ZY2UJm0lKVZyC8aTPfWAI",
+  firebaseConfig : { apiKey: "AIzaSyAufrFfxV2yq8ZY2UJm0lKVZyC8aTPfWAI",
   authDomain: "new23-768df.firebaseapp.com",
   databaseURL: "https://new23-768df-default-rtdb.firebaseio.com",
   projectId: "new23-768df",
   storageBucket: "new23-768df.appspot.com",
   messagingSenderId: "477434350700",
-  appId: "1:477434350700:web:43f449516c033decc91c49"
+  appId: "1:477434350700:web:3c31b64ee25f8a0dc91c49" }
 };
 
 /*
